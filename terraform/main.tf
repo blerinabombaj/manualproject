47fb0ab88d57ce93aac893aa51b59885cddefc3f
Robert Lleshi <robertlleshi@gmail.com>
2026-04-06 22:24:31 +0200

    sync

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,6 +52,7 @@ module "eks" {
   subnet_ids = module.vpc.private_subnets
 
   cluster_endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
 
   eks_managed_node_groups = {
     default = {
